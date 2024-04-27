970e4baaf76edfb8522a51b8bf311df014c21fa3
nikitunya <nikitagorcakov14@gmail.com>
2024-04-28 00:01:54 +0300

    fixed registration screen

--- web/solo-rising/App/screens/SignUpScreen.js
+++ web/solo-rising/App/screens/SignUpScreen.js
@@ -1,86 +1,99 @@
-import React from "react";
-import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { colors } from "../utils/colors";
 import { AntDesign } from "@expo/vector-icons";
 import DismissKeyboard from "../components/DismissKeyboard";
 
 function SignUpScreen() {
   const navigation = useNavigation();
+  const [fullName, setFullName] = useState("Nikita Gorcakovas");
+  const [username, setUsername] = useState("nikitunya");
+  const [email, setEmail] = useState("test@gmail.com");
+  const [password, setPassword] = useState("123456");
+  // const [loading, setLoading] = useState(false);
+
+  const handleSignup = async () => {
+    setLoading(false);
+  }
+
+
   return (
-    <DismissKeyboard>
-      <View
-        className="flex-1 bg-white"
-        style={{ backgroundColor: colors.background }}
-      >
-        <SafeAreaView className="flex">
-          <View className="flex-row justify-start">
-            <TouchableOpacity
-              onPress={() => navigation.goBack()}
-              className="p-2 rounded-tr-2xl rounded-bl-2xl ml-4 mt-2"
-              style={{ backgroundColor: colors.mainGreen }}
-            >
-              <AntDesign name="arrowleft" size={20} color="white" />
-            </TouchableOpacity>
-          </View>
-          <View className="flex-row justify-center">
-            <Image
-              source={require("../utils/images/logo.png")}
-              style={{ width: 200, height: 200 }}
-            />
-          </View>
-        </SafeAreaView>
-        <View
-          className="flex-1 bg-white px-8 pt-8 rounded-t-3xl"
-        >
-          <View className="form space-y-2">
-            <Text className="text-gray-700 ml-4">Full Name</Text>
-            <TextInput
-              className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-              value="john snow"
-              placeholder="Enter Name"
-            />
-            <Text className="text-gray-700 ml-4">Username</Text>
-            <TextInput
-              className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-              value="nikitunya"
-              placeholder="Enter username"
-            />
-            <Text className="text-gray-700 ml-4">Email Address</Text>
-            <TextInput
-              className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
-              value="john@gmail.com"
-              placeholder="Enter Email"
-            />
-            <Text className="text-gray-700 ml-4">Password</Text>
-            <TextInput
-              className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-7"
-              secureTextEntry
-              value="test12345"
-              placeholder="Enter Password"
-            />
-            <TouchableOpacity
-              onPress={() => navigation.navigate("SignUpSecond")}
-              style={{
-                backgroundColor: colors.mainGreen,
-              }}
-              className="py-3 rounded-xl justify-center items-center flex-row"
-            >
-              <Text className="font-xl font-bold text-center text-white">
-                Next
-              </Text>
-              <AntDesign
-                name="right"
-                size={24}
-                color="white"
-                className="ml-5"
+    <KeyboardAvoidingView
+      className="flex-1"
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <DismissKeyboard>
+        <View className="flex-1 justify-between bg-black">
+          <SafeAreaView className="p-2">
+            <View className="flex-row justify-start">
+              <TouchableOpacity
+                onPress={() => navigation.goBack()}
+                className="p-2 rounded-tr-2xl rounded-bl-2xl ml-4 mt-2 bg-blue-700"
+              >
+                <AntDesign name="arrowleft" size={20} color="white" />
+              </TouchableOpacity>
+            </View>
+          </SafeAreaView>
+          <View className="bg-white p-6 rounded-3xl">
+            <View className="form space-y-2">
+              <Text className="text-gray-700 ml-4">Full Name</Text>
+              <TextInput
+                className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
+                placeholder="Enter Full Name"
+                value={fullName}
+                onChangeText={setFullName}
               />
-            </TouchableOpacity>
+              <Text className="text-gray-700 ml-4">Username</Text>
+              <TextInput
+                className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
+                placeholder="Enter username"
+                value={username}
+                onChangeText={setUsername}
+              />
+              <Text className="text-gray-700 ml-4">Email Address</Text>
+              <TextInput
+                className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-3"
+                placeholder="Enter Email"
+                value={email}
+                onChangeText={setEmail}
+              />
+              <Text className="text-gray-700 ml-4">Password</Text>
+              <TextInput
+                className="p-4 bg-gray-100 text-gray-700 rounded-2xl mb-7"
+                secureTextEntry
+                placeholder="Enter Password"
+                value={password}
+                onChangeText={setPassword}
+              />
+              <TouchableOpacity
+                // onPress={() => navigation.navigate("SignUpSecond")}
+                onPress={handleSignup}
+
+                className="py-3 rounded-xl justify-center items-center flex-row mb-5 bg-blue-700"
+              >
+                <Text className="font-xl font-bold text-center text-white">
+                  Next
+                </Text>
+                <AntDesign
+                  name="right"
+                  size={24}
+                  color="white"
+                  className="ml-5"
+                />
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
-      </View>
-    </DismissKeyboard>
+      </DismissKeyboard>
+    </KeyboardAvoidingView>
   );
 }
 
---
+++ web/solo-rising/services/firebase.config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDUeloQDu3m91iS1dmqwBxSGYcwHysyZK0",
+  authDomain: "solo-rising.firebaseapp.com",
+  projectId: "solo-rising",
+  storageBucket: "solo-rising.appspot.com",
+  messagingSenderId: "205232148474",
+  appId: "1:205232148474:web:b839619241de343ab71e43",
+  measurementId: "G-K3D2C8CY55"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { db, app, auth }
