b3bcd1cfaa138ce27dcbdf3be8a77b60a2cfca8e
nikitunya <nikitagorcakov14@gmail.com>
2024-05-29 22:24:29 +0300

    fixed test

--- web/solo-rising/App/tests/WorkoutReview.test.js
+++ web/solo-rising/App/tests/WorkoutReview.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { fireEvent, render, waitFor } from "@testing-library/react-native";
 import WorkoutPreviewModal from "../screens/workout/WorkoutPreviewModal";
+import { useNavigation } from "@react-navigation/native";
+import { ROUTES } from "../constants";
 
 jest.mock("@react-navigation/native", () => ({
   useNavigation: () => ({
@@ -10,27 +12,24 @@ jest.mock("@react-navigation/native", () => ({
 
 describe("WorkoutPreviewModal", () => {
   it("displays workout preview modal with workout information", async () => {
-    // const mockWorkout = {
-    //   exerciseList: [
-    //     { name: "Exercise 1", primary_muscles: "Biceps" },
-    //     { name: "Exercise 2", primary_muscles: "Triceps" },
-    //   ],
-    // };
+    const mockWorkout = {
+      exerciseList: [
+        { name: "Exercise 1", primary_muscles: "Biceps" },
+        { name: "Exercise 2", primary_muscles: "Triceps" },
+      ],
+    };
 
-    // const { getByText } = render(
-    //   <WorkoutPreviewModal modal={true} onClose={() => {}} workout={mockWorkout} />
-    // );
+    const { getByText } = render(
+      <WorkoutPreviewModal modal={true} onClose={() => {}} workout={mockWorkout} />
+    );
 
-    // expect(getByText("Workout Preview")).toBeDefined();
+    await waitFor(() => {
+      expect(getByText("Workout Preview")).toBeDefined();
+      expect(getByText("Exercise 1")).toBeDefined();
+      expect(getByText("Exercise 2")).toBeDefined();
+    });
 
-    // expect(getByText("Biceps")).toBeDefined();
-    // expect(getByText("Triceps")).toBeDefined();
-
-    // expect(getByText("Exercise 1")).toBeDefined();
-    // expect(getByText("Exercise 2")).toBeDefined();
-
-    // fireEvent.press(getByText("Start"));
-
-    // expect(navigation.navigate).toHaveBeenCalledWith("WORKOUT_START", { workout: mockWorkout });
+    fireEvent.press(getByText("Start"));
+    const navigation = useNavigation();
   });
 });
